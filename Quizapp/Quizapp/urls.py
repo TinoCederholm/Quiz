d15a7ec2d86bc3a51cd58be97b09c4607332f15e
TinoCederholm <42689@edu.raseko.fi>
2021-10-28 14:32:46 +0300

    admin,forms,models,views,urls muokattu/lisätty

--- Quizapp/Quizapp/urls.py
+++ Quizapp/Quizapp/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home,name='home'),
+    path('addQuestion/', addQuestion,name='addQuestion'),
+    path('login/', loginPage,name='login'),
+    path('logout/', logoutPage,name='logout'),
+    path('register/', registerPage,name='register'),
+
 ]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
