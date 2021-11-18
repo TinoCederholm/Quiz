9f516f1d6c435ac3f55aabdcba3769dd9a586859
JouniKiiskila <77729162+JouniKiiskila@users.noreply.github.com>
2021-11-18 19:32:09 +0200

    Luotu Superuser tunnukset quiz-kantaan. USER: quiz, PASSWORD: Q2werty

--- DjangoQuiz/settings.py
+++ DjangoQuiz/settings.py
@@ -74,6 +74,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql',
