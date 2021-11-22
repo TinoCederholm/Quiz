d9138ee53c350368c54648b910898ac752077566
JouniKiiskila <77729162+JouniKiiskila@users.noreply.github.com>
2021-11-22 14:16:05 +0200

    Lisätty scripti datan lisäämiseksi

---
+++ Quiz/add_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO "Quiz_quesmodel" ("id","question","op1","op2","op3","op4","ans") VALUES (1,'Mikä on englanninksi lentokone?','Jet','Lawnmower','Car','Airplane','option4');
+INSERT INTO "Quiz_quesmodel" ("id","question","op1","op2","op3","op4","ans") VALUES (2,'Mikä on englanniksi Parturi?','Tuner shop','Chop shop','Barber shop','Car shop','option3');
+INSERT INTO "Quiz_quesmodel" ("id","question","op1","op2","op3","op4","ans") VALUES (3,'Mikä on englanniksi Ruokakauppa?','Clothing store','Electronics store','Hardware store','Grocery store','option4');
+INSERT INTO "Quiz_quesmodel" ("id","question","op1","op2","op3","op4","ans") VALUES (4,'Mikä on englanniksi poliisilaitos?','Police department','Fire department','Department of justice','Coast guard','option1');
+INSERT INTO "Quiz_quesmodel" ("id","question","op1","op2","op3","op4","ans") VALUES (5,'Mikä on englanniksi polkupyörä?','Motorcycle','Bicycle','Tricycle','Auxiliary wheel','option2');
+COMMIT;
